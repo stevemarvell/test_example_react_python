8c36dbe6da75a58ebafb17868990880b92978cb3
Steve Marvell <stevemarvell@crucialtechnical.com>
2023-08-07 22:57:54 +0100

    change exception handler

--- backend/src/infrastructure/rest/api.py
+++ backend/src/infrastructure/rest/api.py
@@ -1,7 +1,6 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi.exceptions import RequestValidationError
 from fastapi.middleware.cors import CORSMiddleware
-from flask import Request
 from starlette import status
 from starlette.responses import JSONResponse
 
@@ -9,14 +8,12 @@ from infrastructure.rest.routes.greeter import router as greeter_router
 
 api = FastAPI()
 
-
 @api.exception_handler(RequestValidationError)
 async def validation_exception_handler(request: Request, exc: RequestValidationError):
     return JSONResponse(
         status_code=status.HTTP_400_BAD_REQUEST,
         content={"detail": exc.errors()},
     )
-
 
 origins = [
     "http://localhost",
