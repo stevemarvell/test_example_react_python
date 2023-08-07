d178eaf24e0f5422f64b895d40bf9b3b1a92f004
Steve Marvell <stevemarvell@crucialtechnical.com>
2023-08-07 23:02:29 +0100

    explain placeholder code in domain

--- backend/src/domain/greeting_service.py
+++ backend/src/domain/greeting_service.py
@@ -2,7 +2,13 @@ import json
 import os
 
 
+# @todo DI a repo
 def greeting_by_name(name):
+
+    # this is a hack for demo purposes
+    # the os should not be in the domain
+    # this is all to be done with an injected repo
+    
     module_dir = os.path.dirname(os.path.abspath(__file__))
     config_file_path = os.path.join(module_dir, 'greetings_config.json')
 
