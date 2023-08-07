5774715507b9bc796cf12b337f5ed16d6be6b979
Steve Marvell <stevemarvell@crucialtechnical.com>
2023-08-07 22:37:42 +0100

    test harness change

--- backend/tests/api/rest/test_rest_api.py
+++ backend/tests/api/rest/test_rest_api.py
@@ -1,26 +1,38 @@
 import pytest
-import subprocess
-import requests
+from fastapi.testclient import TestClient
+from infrastructure.rest.api import api
+
+@pytest.fixture
+def client():
+    return TestClient(api)
 
 
-# @todo fix this
-@pytest.fixture(scope="module", autouse=True)
-def start_app():
-    # Start your FastAPI application as a separate process on port 5000
-    app_process = subprocess.Popen(["uvicorn", "infrastructure.rest.api:api", "--host", "127.0.0.1", "--port", "5000"])
-
-    # Wait for the application to start
-    requests.get("http://127.0.0.1:5000")  # Make a dummy request to ensure the app is up and running
-
-    yield  # This allows the tests to run
-
-    # Terminate the FastAPI application process after all tests have finished
-    app_process.terminate()
+def test_default_greeting(client):
+    response = client.get('/greet?name=Alice')
+    assert response.status_code == 200
+    assert response.json() == {"greeting": "Hi"}
 
 
-def test_invalid_endpoint():
-    response = requests.get('http://127.0.0.1:5000/')
+def test_custom_greeting_for_bob(client):
+    response = client.get('/greet?name=Bob')
+    assert response.status_code == 200
+    assert response.json() == {"greeting": "Hello"}
+
+
+def test_invalid_endpoint(client):
+    response = client.get('/')
     assert response.status_code == 404
 
-    response = requests.get('http://127.0.0.1:5000/boink')
+    response = client.get('/boink')
     assert response.status_code == 404
+
+
+def test_invalid_args(client):
+    response = client.get('/greet')
+    assert response.status_code == 400
+
+    response = client.get('/greet?name=')
+    assert response.status_code == 400
+
+    response = client.get('/greet?cabbage=')
+    assert response.status_code == 400
