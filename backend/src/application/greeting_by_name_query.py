569d59e2ef6364fd2ab8a4d9cacd9ad43baea0fb
Steve Marvell <stevemarvell@crucialtechnical.com>
2023-08-10 17:24:54 +0100

    done is better than perfect

--- backend/src/application/greeting_by_name_query.py
+++ backend/src/application/greeting_by_name_query.py
@@ -1,4 +1,17 @@
+from schema import Schema
+
+from domain import greeting_service
 from domain.greeting_repository import GreetingRepository
-from domain.greeting_service import greeting_by_name
-def handle(greeting_repository: GreetingRepository, name: str) -> str:
-    return greeting_by_name(greeting_repository, name)
+
+
+def validate(query):
+    schema = Schema({
+        'name': lambda n: isinstance(n, str) and len(n) >= 2
+    })
+
+    schema.validate(query)
+
+
+def handle(greeting_repository: GreetingRepository, query: dict) -> str:
+    validate(query)
+    return greeting_service.greeting_by_name(greeting_repository, query['name'])
--- backend/tests/infrastructure/rest/test_rest_api_unit.py
+++ backend/tests/infrastructure/rest/test_rest_api_unit.py
@@ -4,21 +4,27 @@ from unittest.mock import Mock, patch
 
 from infrastructure.rest.routes.greeter import greet
 
-
-def test_greet_user_valid_name():
+@pytest.fixture()
+def mock_greeting_repository():
+    return Mock()
+def test_greet_user_valid_name(mock_greeting_repository):
 
     expected_greeting = "Cabbage"
 
-    mock_greeting_repository = Mock()
     mock_greeting_repository.get_greeting_by_name.return_value = expected_greeting
 
     response = greet("Bob", mock_greeting_repository)
     assert response == {"greeting": expected_greeting}
 
 
-def test_greet_user_invalid_name():
+def test_greet_user_invalid_name(mock_greeting_repository):
+    with pytest.raises(HTTPException) as exc_info:
+        greet("X", mock_greeting_repository)
+
+    assert exc_info.value.status_code == 400
+
+def test_greet_user_blank_name():
     with pytest.raises(HTTPException) as exc_info:
         greet(None)
 
     assert exc_info.value.status_code == 400
-    assert exc_info.value.detail == "Invalid or missing 'name' parameter"
