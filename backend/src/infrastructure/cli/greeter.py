d7df09d825ef8b22cf5aa08558354dffad1cfaeb
Steve Marvell <stevemarvell@crucialtechnical.com>
2023-08-11 13:32:29 +0100

    done is better than perfect (#3)

--- backend/src/infrastructure/cli/greeter.py
+++ backend/src/infrastructure/cli/greeter.py
@@ -1,4 +1,7 @@
+import sys
+
 import click
+from schema import SchemaError
 
 from di import dependency_manager
 
@@ -9,8 +12,17 @@ from application import greeting_by_name_query
 @click.argument('name')
 def main(name):
     greeting_repository = dependency_manager.greeting_repository()
-    greeting = greeting_by_name_query.handle(greeting_repository, name)
-    click.echo(f'{greeting}, {name}!')
+
+    try:
+        greeting = greeting_by_name_query.handle(greeting_repository, { "name": name })
+        click.echo(f'{greeting}, {name}!')
+    except SchemaError as e:
+        click.echo(f"Well that didn't validate!")
+        sys.exit(1)
+    except Exception as e:
+        click.echo(f"Well, that didn't work at all!")
+        sys.exit(2)
+
 
 
 if __name__ == '__main__':
