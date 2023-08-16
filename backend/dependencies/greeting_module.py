a3a92f22d0dd9c554ef41e29beb9a050d9791c23
Steve Marvell <stevemarvell@crucialtechnical.com>
2023-08-16 18:39:35 +0100

    interim

--- backend/dependencies/greeting_module.py
+++
@@ -1,9 +0,0 @@
-from injector import Binder, Module
-
-from domain.greeting_repository import GreetingRepository
-from infrastructure.repositories.greeting_repository_array import GreetingRepositoryArray
-
-
-class GreetingModule(Module):
-    def configure(self, binder: Binder) -> None:
-        binder.bind(GreetingRepository, to=GreetingRepositoryArray)
--- backend/di.py
+++ backend/di.py
@@ -1,24 +1,19 @@
-from functools import wraps
-
-from injector import Injector
+from injector import Injector, Module, Binder
 
 from domain.greeting_repository import GreetingRepository
-from dependencies.greeting_module import GreetingModule  # Import your existing module
 from infrastructure.repositories.greeting_repository_array import GreetingRepositoryArray
 
+def configure(binder):
+    # if test, dev, prod, etc
+    binder.bind(GreetingRepository, to=GreetingRepositoryArray)
 
 class DependencyManager:
     def __init__(self):
-        self.injector = Injector(modules=[GreetingModule()])
+        self.injector = Injector([configure])
 
     def greeting_repository(self):
         return self.injector.get(GreetingRepository)
 
-
 # Create a single instance of DependencyManager
 dependency_manager = DependencyManager()
 
-def configure(binder):
-    # if test, dev, prod, etc
-    repository = GreetingRepositoryArray()
-    binder.bind(GreetingRepository, to=repository)
